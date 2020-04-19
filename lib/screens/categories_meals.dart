1f0d0133730ce47c068a55d6e3f07e361df07a3e
luccasoli <oliveira.mesquita@aluno.uece.br>
2020-04-19 18:41:51 -0300

    listing meals of a categorie

--- lib/screens/categories_meals.dart
+++ lib/screens/categories_meals.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:meals_flutter/data/dummy_data.dart';
 import 'package:meals_flutter/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Category category = ModalRoute.of(context).settings.arguments;
+    final categoryMeals = DUMMY_MEALS
+        .where(
+          (meal) => meal.categories.contains(category.id),
+        )
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas por categoria'),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
