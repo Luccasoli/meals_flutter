afe0dc9230de0c6fb932221e71982079bd707922
luccasoli <oliveira.mesquita@aluno.uece.br>
2020-04-19 22:28:26 -0300

    listing recipe steps

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -2,6 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:meals_flutter/models/meal.dart';
 
 class MealDetailsScreen extends StatelessWidget {
+  Widget _createSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        vertical: 10,
+      ),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
+      ),
+    );
+  }
+
+  Widget _createSectionContainer(BuildContext context, Widget child) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: 200,
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final Meal meal = ModalRoute.of(context).settings.arguments;
@@ -11,43 +38,28 @@ class MealDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Column(
-        children: <Widget>[
-          ConstrainedBox(
-            constraints: BoxConstraints(
-              maxHeight: MediaQuery.of(context).size.height * 0.4,
-            ),
-            child: Container(
-              width: double.infinity,
-              child: Hero(
-                tag: meal.id,
-                child: Image.network(
-                  meal.imageUrl,
-                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                maxHeight: MediaQuery.of(context).size.height * 0.4,
+              ),
+              child: Container(
+                width: double.infinity,
+                child: Hero(
+                  tag: meal.id,
+                  child: Image.network(
+                    meal.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(
-              vertical: 10,
-            ),
-            child: Text(
-              'Ingredientes',
-              style: theme.textTheme.title,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: 300,
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: ListView.builder(
+            _createSectionTitle(context, 'Ingredientes'),
+            _createSectionContainer(
+              context,
+              ListView.builder(
                 itemBuilder: (context, index) {
                   return Card(
                     child: Padding(
@@ -63,8 +75,30 @@ class MealDetailsScreen extends StatelessWidget {
                 itemCount: meal.ingredients.length,
               ),
             ),
-          ),
-        ],
+            _createSectionTitle(context, 'Passos'),
+            _createSectionContainer(
+              context,
+              ListView.builder(
+                itemCount: meal.steps.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('${index + 1}'),
+                        ),
+                        title: Text(
+                          meal.steps[index],
+                        ),
+                      ),
+                      Divider(),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
